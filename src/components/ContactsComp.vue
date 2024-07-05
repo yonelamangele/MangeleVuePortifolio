72d1336100e25b8a365ba6318677defdcfd457ca
Yonela <yonelamnagele1700@gmail.com>
2024-07-05 16:53:02 +0200

    added files

--- src/components/ContactsComp.vue
+++ src/components/ContactsComp.vue
@@ -1,14 +1,16 @@
 <template>
     <div>
-        <form  action="https://formcarry.com/s/koOwMO21l5o" method="POST" enctype="multipart/form-data">
+        <form  action="https://formcarry.com/s/zIb-s6L1api"
+  method="POST"
+  enctype="multipart/form-data">
             <label> Name: </label>
             <input type="name" placeholder="eg. Cristiano Ronaldo">
             
             <label> Email: </label>
-            <input type="email" v-model="email" placeholder="eg. example@gmail.com">
+            <input type="email" placeholder="eg. example@gmail.com">
             
             <label> Mesage: </label>
-            <textarea type="text" v-model="message" placeholder="Type Your Message Here"> </textarea>
+            <textarea type="text" placeholder="Type Your Message Here"> </textarea>
             
             <section id="mainButtons">
                 
